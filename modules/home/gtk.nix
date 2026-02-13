dc253e0deb0d162a01d3dbfc10934ef979d139d8
Krish-jswl <imkrishjaiswal05@gmail.com>
2026-02-14 03:16:09 +0530

    gruvbox theme

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -1,12 +1,27 @@
 { pkgs, ... }:
 
+let
+  gruvboxSoft =
+    pkgs.gruvbox-gtk-theme.overrideAttrs (old: {
+      installPhase = ''
+        mkdir -p $out/share/themes
+        cd themes
+        bash install.sh \
+          -d $out/share/themes \
+          -c dark \
+          -t default \
+          --tweaks soft
+      '';
+    });
+in
+
 {
   gtk = {
     enable = true;
 
     theme = {
-      name = "Gruvbox-Dark";
-      package = pkgs.gruvbox-gtk-theme;
+      name = "gruvbox-gtk-theme-0-unstable-2025-10-23-Dark-Soft";
+      package = gruvboxSoft;
     };
 
     iconTheme = {
